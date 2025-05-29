97f7bf260bf34502024b459239ffb19365f6a5e8
Elyor Yadgarov <eyodgorov@gmail.com>
2025-05-28 23:06:37 -0400

    Variable creating and printing

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Console.WriteLine("Hello, World!");
+            // Declare and initialize variables for the following types:
+            // string
+            // int
+            // char
+            // bool
+            // double
+            // decimal
+
+            string myName = "Elyor";
+            int myAge = 40;
+            char gender = 'm';
+            bool myIsMale = true;
+            double someValue = 83.02;
+            decimal someDecimalValue = 3.14159265358979323846M;
+            Console.WriteLine($"Hello team my name is: {myName} my age is {myAge} and gender is: {gender}");
+            Console.WriteLine($"if don't believe you can check: {myIsMale}, temp outside today is: {someValue}, below is some numbers: {someDecimalValue}.");
+
         }
     }
 }
